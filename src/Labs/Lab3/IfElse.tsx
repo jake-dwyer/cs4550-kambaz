8845aff8edc4e08ba8226d992f2a12af8310ae0f
jake-dwyer <dwyer.jak@northeastern.edu>
2025-02-19 18:55:31 -0500

    fix push

--- src/Labs/Lab3/IfElse.tsx
+++ src/Labs/Lab3/IfElse.tsx
@@ -1,10 +1,6 @@
 export default function IfElse() {
-    let numberVariable = 123, floatingPointNumber =234.345;
-    let true1 = true, false1 = false, false2 = true1 && false1;
-    let true2 = true1 || false1, true3 = !false2;
-    let true4 = numberVariable === 123;
-    let true5 = floatingPointNumber !== 321.432;
-    let false3 = numberVariable < 100;
+    let true1 = true, false1 = false;
+
     return (
         <div id="wd-if-else">
             <h4>If Else</h4>
