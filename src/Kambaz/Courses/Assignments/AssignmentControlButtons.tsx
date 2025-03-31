ab30ee3277e40b2b5b8adf2d8fe07868b94d355b
jake-dwyer <dwyer.jak@northeastern.edu>
2025-03-31 18:45:41 -0400

    a4 fix

--- src/Kambaz/Courses/Assignments/AssignmentControlButtons.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentControlButtons.tsx
@@ -4,25 +4,29 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AssignmentControlButtons() {
-    const navigate = useNavigate();
-    const { cid } = useParams();
-    const user = useSelector((state: any) => state.user);
-    const canEdit = user && (user.role === "ADMIN" || user.role === "FACULTY");
+  const navigate = useNavigate();
+  const { cid } = useParams();
 
-    if (!canEdit) {
-        return null;
-    }
-    
-    return (
-        <div className="float-end">
-            <div id="wd-total" className="d-inline-block rounded-pill border border-dark px-2 me-2">
-                40% of Total
-            </div>
-            <FaPlus 
-                className="fs-4 cursor-pointer" 
-                onClick={() => navigate(`/Kambaz/Courses/${cid}/Assignments/new`)} 
-            />
-            <IoEllipsisVertical className="fs-4" />
-        </div>
-    );
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const canEdit = currentUser && (currentUser.role === "ADMIN" || currentUser.role === "FACULTY");
+
+  if (!canEdit) {
+    return null;
+  }
+
+  return (
+    <div className="float-end">
+      <div
+        id="wd-total"
+        className="d-inline-block rounded-pill border border-dark px-2 me-2"
+      >
+        40% of Total
+      </div>
+      <FaPlus
+        className="fs-4 cursor-pointer"
+        onClick={() => navigate(`/Kambaz/Courses/${cid}/Assignments/new`)}
+      />
+      <IoEllipsisVertical className="fs-4" />
+    </div>
+  );
 }
