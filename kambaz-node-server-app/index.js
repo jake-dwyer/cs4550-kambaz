a5c3d9f2b86d7e17c0a1d837a540e3fae1d49187
jake-dwyer <dwyer.jak@northeastern.edu>
2025-04-01 17:49:42 -0400

    ended at page 175

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -1,10 +1,35 @@
-import express from 'express'
+import express from 'express';
+import session from "express-session";
+import "dotenv/config";
+import UserRoutes from './Kambaz/Users/routes.js';
 import Hello from "./Hello.js"
 import cors from "cors";
 import Lab5 from "./Labs/Lab5/index.js";
+
 const app = express()
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin:  process.env.NETLIFY_URL || "http://localhost:5173",
+    })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+app.use(session(sessionOptions));  
 app.use(express.json());
+const port = process.env.PORT || 4000;
+UserRoutes(app);
 Lab5(app)
 Hello(app)
-app.listen(process.env.PORT || 4000)
+app.listen(port)
