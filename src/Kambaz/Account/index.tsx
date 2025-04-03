189e68b5eba13598896f9fb5bd1964c1d52d6cb4
jake <jakedwyer7@gmail.com>
2025-04-02 22:54:51 -0400

    qol

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -7,21 +7,36 @@ import { useSelector } from "react-redux";
 
 export default function Kambaz() {
     const { currentUser } = useSelector((state: any) => state.accountReducer);
+  
     return (
-        <div id="account-screen">
-            <table>
-                <tr>
-                    <td valign="top"> <AccountNavigation /> </td>
-                    <td valign="top">
-                        <Routes>
-                            <Route path="/" element={<Navigate to={currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin"} />} />
-                            <Route path="/Signin" element={<Signin />} />
-                            <Route path="/Profile" element={<Profile />} />
-                            <Route path="/Signup" element={<Signup />} />
-                        </Routes>
-                    </td>
-                </tr>
-            </table>
-        </div>
+      <div id="account-screen">
+        <table>
+          <tbody>
+            <tr>
+              <td valign="top"> <AccountNavigation /> </td>
+              <td valign="top">
+                <Routes>
+                  <Route
+                    path="/"
+                    element={
+                      <Navigate
+                        to={
+                          currentUser
+                            ? "/Kambaz/Account/Profile"
+                            : "/Kambaz/Account/Signin"
+                        }
+                      />
+                    }
+                  />
+                  <Route path="/Signin" element={<Signin />} />
+                  <Route path="/Profile" element={<Profile />} />
+                  <Route path="/Signup" element={<Signup />} />
+                </Routes>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     );
-}
+  }
+  
